e3319c28398b940f64a2a074f2da0b30f0977aeb
HaoSun <sunhao.private@foxmail.com>
2016-10-25 15:46:13 +0800

    Initial Commit

---
+++ MTPublicCell/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MTPublicCell
+//
+//  Created by HaoSun on 16/10/25.
+//  Copyright © 2016年 MaiTian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MTPublicCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTPublicCell
+//
+//  Created by HaoSun on 16/10/25.
+//  Copyright © 2016年 MaiTian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
